1c2c1498147fbf08c59bdf6258bfaa1ea9a87be5
Dámaso Armas <127236212+dasktorm@users.noreply.github.com>
2023-06-09 15:27:28 +0000

    final-version

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function generateDomainNames(pronouns, adjectives, nouns) {
+  let domainNames = [];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  for (let i = 0; i < pronouns.length; i++) {
+    for (let j = 0; j < adjectives.length; j++) {
+      for (let k = 0; k < nouns.length; k++) {
+        let domainName = pronouns[i] + adjectives[j] + nouns[k] + ".com";
+        domainNames.push(domainName);
+      }
+    }
+  }
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  return domainNames;
+}
+
+let pronouns = ["the", "our"];
+let adjectives = ["great", "big"];
+let nouns = ["jogger", "racoon"];
+
+let generateButton = document.getElementById("generateButton");
+generateButton.addEventListener("click", function(event) {
+  event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+
+  let domainNames = generateDomainNames(pronouns, adjectives, nouns);
+  console.log(domainNames);
+});
